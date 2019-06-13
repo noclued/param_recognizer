e02235225891c7846ac78d1d7751835c95474d02
Konrad Kosowski <k.kosowski@i-systems.pl>
2019-06-13 12:36:00 +0200

    make it more universal

--- src/ParamRecognizer.php
+++ src/ParamRecognizer.php
@@ -11,14 +11,23 @@ class ParamRecognizer
         $this->params = $params;
     }
 
-    public function getStandardFilePath()
+    public function __call($name, $arguments)
     {
-        return $this->getValueForKey('standard');
+        $key = lcfirst(str_replace("get", "", $name));
+
+        return $this->getValueForKey($key);
     }
 
-    private function getValueForKey($key) : ?string
+    private function getValueForKey(string $key) : ?string
     {
+        $originalKey = $key;
         $key = sprintf("--%s=", $key);
+        foreach ($this->params as $value) {
+            if (false !== strpos($value, $key)) {
+                return str_replace($key, '', $value);
+            }
+        }
+        $key = sprintf("--%s=", $this->fromCamelCase($originalKey));
         foreach ($this->params as $value) {
             if (false !== strpos($value, $key)) {
                 return str_replace($key, '', $value);
@@ -26,5 +35,16 @@ class ParamRecognizer
         }
 
         return null;
+    }
+
+    private function fromCamelCase(string $input)
+    {
+         preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+         $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+
+        return implode('_', $ret);
     }
 }
